feb392b6cb8c6baf0c516b6066cdb11bf10de396
Pakk1e <72614201+Pakk1e@users.noreply.github.com>
2026-09-09 12:53:45 +0200

    Export inductor transient model

--- worlds/src/worlds/simulation/__init__.py
+++ worlds/src/worlds/simulation/__init__.py
@@ -3,193 +3,24 @@ from .circuit import (
     CircuitInstance,
     SimulationCircuit,
 )
-
-from .binding import (
-    BoundEquation,
-    SimulationBindingError,
-    bind_component_equations,
-)
-
-from .builder import (
-    SimulationBuildError,
-    build_simulation_component,
-)
-
-from .equations import (
-    EquationSystem,
-    EquationSystemError,
-    SimulationEquation,
-    build_equation_system,
-    collect_unknowns,
-)
-
-from .model import (
-    SimulationComponent,
-    SimulationModel,
-)
-
-from .network import (
-    NetworkError,
-    build_network_equation_system,
-)
-
-from .analysis import (
-    DC_OPERATING_POINT,
-    DC_SWEEP,
-    TRANSIENT,
-    SUPPORTED_ANALYSES,
-    SimulationAnalysisError,
-    SimulationConfiguration,
-    DCOperatingPointAnalysis,
-    DCSweepAnalysis,
-    DCSweepResult,
-    TransientAnalysis,
-    TransientResult,
-    get_simulation_analysis,
-)
-
-from .transient import (
-    TransientAnalysisError,
-    TransientConfigurationError,
-    TransientConfiguration,
-    parse_transient_settings,
-    build_time_points,
-)
-
-from .state import (
-    DynamicState,
-    DynamicStateError,
-    DynamicStateSnapshot,
-    TransientStepContext,
-    TransientStateHandler,
-    NoOpTransientStateHandler,
-)
-
-from .dynamic import (
-    DynamicComponentError,
-    CapacitorTransientModel,
-    CapacitorStateHandler,
-    TransientDynamicStateHandler,
-)
-
-from .representation import (
-    ComponentRepresentation,
-    ComponentRepresentationError,
-    ElectricalComponentRepresentation,
-)
-
-from .request import (
-    SimulationRequest,
-    SimulationRequestError,
-)
-
-from .result import (
-    SimulationDataset,
-    SimulationResultModel,
-)
-
-from .session import (
-    SimulationSession,
-    SimulationSessionError,
-    SimulationSessionSnapshot,
-    SimulationSessionStatus,
-)
-
-from .simulate import (
-    SimulationError,
-    simulate,
-)
-
-from .solver import (
-    LinearSolver,
-    SimulationResult,
-    SimulationSolver,
-    SolveResult,
-    SolverError,
-    SimulationInstanceResult,
-)
-
-from .service import (
-    SimulationResponse,
-    SimulationService,
-    SimulationServiceError,
-)
-
+from .binding import (BoundEquation, SimulationBindingError, bind_component_equations)
+from .builder import (SimulationBuildError, build_simulation_component)
+from .equations import (EquationSystem, EquationSystemError, SimulationEquation, build_equation_system, collect_unknowns)
+from .model import (SimulationComponent, SimulationModel)
+from .network import (NetworkError, build_network_equation_system)
+from .analysis import (DC_OPERATING_POINT, DC_SWEEP, TRANSIENT, SUPPORTED_ANALYSES, SimulationAnalysisError, SimulationConfiguration, DCOperatingPointAnalysis, DCSweepAnalysis, DCSweepResult, TransientAnalysis, TransientResult, get_simulation_analysis)
+from .transient import (TransientAnalysisError, TransientConfigurationError, TransientConfiguration, parse_transient_settings, build_time_points)
+from .state import (DynamicState, DynamicStateError, DynamicStateSnapshot, TransientStepContext, TransientStateHandler, NoOpTransientStateHandler)
+from .dynamic import (DynamicComponentError, CapacitorTransientModel, CapacitorStateHandler, InductorTransientModel, InductorStateHandler, TransientDynamicStateHandler)
+from .representation import (ComponentRepresentation, ComponentRepresentationError, ElectricalComponentRepresentation)
+from .request import (SimulationRequest, SimulationRequestError)
+from .result import (SimulationDataset, SimulationResultModel)
+from .session import (SimulationSession, SimulationSessionError, SimulationSessionSnapshot, SimulationSessionStatus)
+from .simulate import (SimulationError, simulate)
+from .solver import (LinearSolver, SimulationResult, SimulationSolver, SolveResult, SolverError, SimulationInstanceResult)
+from .service import (SimulationResponse, SimulationService, SimulationServiceError)
 
 __all__ = [
-    "BoundEquation",
-    "CircuitError",
-    "CircuitInstance",
-    "EquationSystem",
-    "EquationSystemError",
-    "LinearSolver",
-    "NetworkError",
-    "SimulationAnalysisError",
-    "SimulationBindingError",
-    "SimulationBuildError",
-    "SimulationComponent",
-    "SimulationCircuit",
-    "SimulationConfiguration",
-    "SimulationDataset",
-    "SimulationRequest",
-    "SimulationRequestError",
-    "SimulationSession",
-    "SimulationSessionError",
-    "SimulationSessionSnapshot",
-    "SimulationSessionStatus",
-    "DC_OPERATING_POINT",
-    "DC_SWEEP",
-    "TRANSIENT",
-    "DCOperatingPointAnalysis",
-    "DCSweepAnalysis",
-    "DCSweepResult",
-    "TransientAnalysis",
-    "TransientResult",
-    "SUPPORTED_ANALYSES",
-    "SimulationEquation",
-    "SimulationError",
-    "SimulationModel",
-    "SimulationResult",
-    "SimulationResultModel",
-    "SimulationSolver",
-    "SolveResult",
-    "SolverError",
-    "bind_component_equations",
-    "build_equation_system",
-    "build_network_equation_system",
-    "build_simulation_component",
-    "get_simulation_analysis",
-    "TransientAnalysisError",
-    "TransientConfigurationError",
-    "TransientConfiguration",
-    "parse_transient_settings",
-    "build_time_points",
-    "DynamicState",
-    "DynamicStateError",
-    "DynamicStateSnapshot",
-    "TransientStepContext",
-    "TransientStateHandler",
-    "NoOpTransientStateHandler",
-    "DynamicComponentError",
-    "CapacitorTransientModel",
-    "CapacitorStateHandler",
-    "TransientDynamicStateHandler",
-    "ComponentRepresentation",
-    "ComponentRepresentationError",
-    "ElectricalComponentRepresentation",
-    "SimulationValidationError",
-    "SimulationValidator",
-    "ValidationIssue",
-    "collect_unknowns",
-    "simulate",
-    "SimulationResponse",
-    "SimulationService",
-    "SimulationServiceError",
-    "SimulationInstanceResult",
+    "BoundEquation", "CircuitError", "CircuitInstance", "EquationSystem", "EquationSystemError", "LinearSolver", "NetworkError", "SimulationAnalysisError", "SimulationBindingError", "SimulationBuildError", "SimulationComponent", "SimulationCircuit", "SimulationConfiguration", "SimulationDataset", "SimulationRequest", "SimulationRequestError", "SimulationSession", "SimulationSessionError", "SimulationSessionSnapshot", "SimulationSessionStatus", "DC_OPERATING_POINT", "DC_SWEEP", "TRANSIENT", "DCOperatingPointAnalysis", "DCSweepAnalysis", "DCSweepResult", "TransientAnalysis", "TransientResult", "SUPPORTED_ANALYSES", "SimulationEquation", "SimulationError", "SimulationModel", "SimulationResult", "SimulationResultModel", "SimulationSolver", "SolveResult", "SolverError", "bind_component_equations", "build_equation_system", "build_network_equation_system", "build_simulation_component", "get_simulation_analysis", "TransientAnalysisError", "TransientConfigurationError", "TransientConfiguration", "parse_transient_settings", "build_time_points", "DynamicState", "DynamicStateError", "DynamicStateSnapshot", "TransientStepContext", "TransientStateHandler", "NoOpTransientStateHandler", "DynamicComponentError", "CapacitorTransientModel", "CapacitorStateHandler", "InductorTransientModel", "InductorStateHandler", "TransientDynamicStateHandler", "ComponentRepresentation", "ComponentRepresentationError", "ElectricalComponentRepresentation", "SimulationValidationError", "SimulationValidator", "ValidationIssue", "collect_unknowns", "simulate", "SimulationResponse", "SimulationService", "SimulationServiceError", "SimulationInstanceResult",
 ]
-
-from .validation import (
-    SimulationValidationError,
-    SimulationValidator,
-    ValidationIssue,
-)
+from .validation import SimulationValidationError, SimulationValidator, ValidationIssue
