f3de9bc3162568486e57e310d62a965a44379c1a
Pakk1e <72614201+Pakk1e@users.noreply.github.com>
2026-09-09 21:58:27 +0200

    Extract simulation request and result normalization

---
+++ frontend-dev/src/applications/worlds/model/simulationTransport.js
@@ -0,0 +1,13 @@
+export function buildSimulationRequest(nodes, edges, simulationConfig, serializeWorldGraph) {
+  return {
+    ...serializeWorldGraph(nodes, edges),
+    simulation: simulationConfig,
+  };
+}
+
+export function normalizeSimulationResponse(data) {
+  const componentResults = Array.isArray(data.components)
+    ? Object.fromEntries(data.components.map((component) => [component.id, component]))
+    : data.components ?? {};
+  return { ...data, components: componentResults };
+}
