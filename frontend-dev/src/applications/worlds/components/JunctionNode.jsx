39f02cf2264997fadd750f997f6ff561421e8f8f
Pakk1e <72614201+Pakk1e@users.noreply.github.com>
2026-09-12 22:08:12 +0200

    refactor: clarify junction interaction surface

--- frontend-dev/src/applications/worlds/components/JunctionNode.jsx
+++ frontend-dev/src/applications/worlds/components/JunctionNode.jsx
@@ -1,46 +1,15 @@
 import { Handle, Position } from "@xyflow/react";
 
 const handles = [
-  {
-    id: "junction-top",
-    position: Position.Top,
-    className: "!left-1/2 !top-1/2",
-  },
-  {
-    id: "junction-right",
-    position: Position.Right,
-    className: "!left-1/2 !top-1/2",
-  },
-  {
-    id: "junction-bottom",
-    position: Position.Bottom,
-    className: "!left-1/2 !top-1/2",
-  },
-  {
-    id: "junction-left",
-    position: Position.Left,
-    className: "!left-1/2 !top-1/2",
-  },
+  ["junction-top", Position.Top],
+  ["junction-right", Position.Right],
+  ["junction-bottom", Position.Bottom],
+  ["junction-left", Position.Left],
 ];
 
 export default function JunctionNode() {
-  return (
-    <div className="relative h-4 w-4">
-      <div className="absolute left-1/2 top-1/2 h-3 w-3 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#26364d]" />
-
-      {handles.map((handle) => (
-        <Handle
-          key={handle.id}
-          id={handle.id}
-          type="source"
-          position={handle.position}
-          isConnectable
-          className={[
-            "!h-4 !w-4 !border-0 !bg-transparent",
-            handle.className,
-          ].join(" ")}
-        />
-      ))}
-    </div>
-  );
+  return <div data-testid="junction-node" className="relative h-4 w-4">
+    <div className="absolute left-1/2 top-1/2 h-3 w-3 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#26364d]" />
+    {handles.map(([id, position])=><Handle key={id} id={id} type="source" position={position} isConnectable className="!h-4 !w-4 !border-0 !bg-transparent !left-1/2 !top-1/2" title="Junction"/>)}
+  </div>;
 }
