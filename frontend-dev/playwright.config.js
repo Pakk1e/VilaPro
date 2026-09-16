24cd03891305635e0392f71210afc0fed1b08035
Pakk1e <72614201+Pakk1e@users.noreply.github.com>
2026-09-16 21:21:58 +0200

    test: remove expensive acceptance videos

--- frontend-dev/playwright.config.js
+++ frontend-dev/playwright.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
         viewport: { width: 1440, height: 900 },
         screenshot: "only-on-failure",
         trace: "retain-on-failure",
-        video: "retain-on-failure",
+        video: "off",
         serviceWorkers: "block",
         storageState: "playwright/.auth/user.json",
     },
